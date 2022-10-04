e9f6d9f99682afd4a234ee2100041fb74510dcf7
KuvaevM <shlyabom@gmail.com>
2022-10-04 17:09:44 +0300

    add lost files

--- src/client.py
+++ src/client.py
@@ -1,9 +1,13 @@
-from pydantic import BaseModel
+import grpc
+from protobuf.protobuild.OnlineShop_pb2_grpc import ThingsSearcherStub
+from protobuf.protobuild.OnlineShop_pb2 import TypeThing
 
 
-class News(BaseModel):
-    """Contract for item."""
-
-    name: str
-    description: str | None = None
-    text: str
+def search_things(sub_name_: str, lower_price_: int, upper_price_: int) -> int:
+    """Connects to server, search things"""
+    with grpc.insecure_channel('localhost:4000') as channel:
+        client = ThingsSearcherStub(channel)
+        answer = client.ThingsSearcher(request=TypeThing(sub_name=sub_name_,
+                                                         lower_price=lower_price_,
+                                                         upper_price=upper_price_))
+        return answer.number_things
--- src/db.py
+++ src/db.py
@@ -1,25 +1,4 @@
-import time
+from typing import List
+from content import Thing
 
-from fastapi import FastAPI, Request
-
-from src.routers import router
-
-app = FastAPI(
-    title="BaseApp",
-    description=("BaseApp"),
-    version="0.0.1",
-    docs_url="/docs",
-    redoc_url="/docs/redoc",
-)
-
-app.include_router(router)
-
-
-@app.middleware("http")
-async def add_process_time_header(request: Request, call_next):
-    """What time."""
-    start_time = time.time()
-    response = await call_next(request)
-    process_time = time.time() - start_time
-    response.headers["X-Process-Time"] = str(process_time)
-    return response
+db_things: List[Thing] = []  # contains Thing objects
